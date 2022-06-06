d8de7e5e5408ddbb81951af1067f0d981ef8cf2f
Lornah Mukiri <mukirikabuli@gmail.com>
2022-06-06 23:38:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = (num1 * num2);
+
+
+let random = 1;
+Math.random();
+
+
+let num3 = 10;
+let num4 = 6;
+const mod = (num3 % num4)
+
+
+let max = Math.max(5, 10, 15, 20)
+
